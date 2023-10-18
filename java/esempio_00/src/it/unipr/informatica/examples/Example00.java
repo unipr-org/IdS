617cbc2b08260cefa0c335105eafaf7885c15c97
0xMere <merenda.s.mattia@gmail.com>
2023-10-18 12:17:00 +0200

    @merendamattia: aggiunta commenti

--- java/esempio_00/src/it/unipr/informatica/examples/Example00.java
+++ java/esempio_00/src/it/unipr/informatica/examples/Example00.java
@@ -1,7 +1,7 @@
 package it.unipr.informatica.examples;
 
 public class Example00 {
-	private static void go() {
+	private void go() {
 		for(int j = 0; j < 5; ++j) {
 			Thread a = new ThreadA(j);
 			a.start();
@@ -14,15 +14,71 @@ public class Example00 {
 			
 			Thread d = new Thread(new ThreadD(j));
 			d.start();
+			
+			
+			// Classi interne anonime sono classi interne private solo che possiamo non dargli un nome
+			// Non hanno costruttori
+			final int idE = j;
+			
+			Thread e = new Thread() {
+				// Questo e' il corpo della classe anonima
+				// E' una sottoclasse di Thread di cui non conosco il nome
+				@Override
+				public void run() {
+					// `j` cambia ogni iterazione, quindi la classe non sa quale usare
+					// Devo usare una variabile final
+					print('E', idE);
+				}
+			};
+			e.start();
+			
+			
+			final int idF = j;
+			Thread f = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					print('F', idF);
+					
+					// `this` si riferisce a `Thread`
+					// `Example00.this` si riferisce all'oggetto `this` prima che 
+					// venisse chiamata la `new`
+				}
+			});
+			f.start();
+
+			
+			// Interfaccia funzionale: interfaccia con un solo metodo
+			// Sfrutto queste "lambda" per scrivere solo quello che mi serve
+			//
+			// In questo caso il compilatore capisce che sto implementando 
+			// l'interfaccia `runnable`
+			//
+			// Una lambda exp. e' un oggetto
+			final int idG = j;
+			Thread g = new Thread(() -> {
+				print('G', idG);
+			});
+			g.start();
+			
+			
+			// Questa serve per funzioni che ritornano un risultato
+			final int idH = j;
+			Thread h = new Thread(() -> print('H', idH));
+			h.start();
+			
 		}
 	}
 	
-	public static void main(String[] args) {
-		// System.out.println("Partito");
-		Example00.go();
+	private void print(char letter, int id) {
+		for(int i = 0; i < 10; ++i)
+			System.out.println(letter + ": " + i + " - id: " + id);
 	}
 	
-	// Classe Inner
+	public static void main(String[] args) {
+		new Example00().go();
+	}
+	
+	// Inner Class
 	// Una classe interna statica non ha accesso diretto ai membri 
 	// non statici della classe esterna, ma può accedere solo a membri 
 	// statici della classe esterna, se presenti.
@@ -30,39 +86,30 @@ public class Example00 {
 	// Se dichiaro le classi interne come statiche, posso istanziarle 
 	// direttamente all'interno del metodo `go` o `main` senza dover 
 	// istanziare un oggetto della classe esterna. 
-	//
-	// Se non metto `static` alla classe inner devo usare `Example00.this.id`
-	private static class ThreadC extends Thread {
+	private class ThreadC extends Thread {
 		private int id;
 		
 		private ThreadC(int id) {
-			if(id < 0)
-				throw new IllegalArgumentException("id");
-			
 			this.id = id;
 		}
 		
 		@Override
 		public void run() {
-			for(int i = 0; i < 10; ++i)
-				System.out.println("C: " + i + " - id: " + id);
+			print('C', id);
 		}
 	}
 	
-	private static class ThreadD implements Runnable {
+	// Le classi interne private sono considerate "sporche"
+	private class ThreadD implements Runnable {
 		private int id;
 		
-		public ThreadD(int id) {
-			if(id < 0)
-				throw new IllegalArgumentException("id");
-			
+		private ThreadD(int id) {
 			this.id = id;
 		}
 		
 		@Override
 		public void run() {
-			for(int i = 0; i < 10; ++i)
-				System.out.println("D: " + i + " - id: " + id);
+			print('D', id);
 		}
 	}
 }

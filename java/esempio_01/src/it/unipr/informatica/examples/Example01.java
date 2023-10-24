0d7fb022e493b17ee934b8e172a3c19610b08512
0xMere <merenda.s.mattia@gmail.com>
2023-10-24 11:46:44 +0200

    @merendamattia: aggiunti appunti della giornata odierna

--- java/esempio_01/src/it/unipr/informatica/examples/Example01.java
+++ java/esempio_01/src/it/unipr/informatica/examples/Example01.java
@@ -1,29 +1,40 @@
 package it.unipr.informatica.examples;
 
 public class Example01 {
-	private volatile boolean done = false;
+	private volatile boolean doneA = false;
+	// Per come lo abbiamo scritto `volatile`, anche doneB "eredita" il `volatile`,
+	// perche' l'oggetto Example01 viene caricato in memoria
+	
+	private boolean doneB = false;
+	private boolean doneC = false;
 	
 	private void go() {
-		new Waiter().start();
-		new Notifier().start();
+//		new WaiterA().start();
+//		new NotifierA().start();
+//		
+//		new WaiterB().start();
+//		new NotifierB().start();
+		
+		new WaiterC().start();
+		new NotifierC().start();
 	}
 	
 	public static void main(String[] args) {
 		new Example01().go();
 	}
 	
-	private class Notifier extends Thread {
+	private class NotifierA extends Thread {
 		@Override
 		public void run() {
-			System.out.println("Notifier started");
+			System.out.println("Notifier A started");
 			
 			try {
 				
 				Thread.sleep(5000);
 					
-				done = true;
+				doneA = true;
 					
-				System.out.println("Notified");
+				System.out.println("Notified A");
 				
 			} catch (InterruptedException e) {
 				System.out.println("Interrupted");
@@ -31,14 +42,14 @@ public class Example01 {
 				return;
 			}
 
-			System.out.println("Notifier terminated");
+			System.out.println("Notifier A terminated");
 		}
 	}
 	
-	private class Waiter extends Thread {
+	private class WaiterA extends Thread {
 		@Override
 		public void run() {
-			System.out.println("Waiter started");
+			System.out.println("Waiter A started");
 			// Attesa attiva (busy waiting) perche' aspetto e guardo, aspetto e guardo, aspetto e guardo (in loop)
 		
 			do {
@@ -56,9 +67,100 @@ public class Example01 {
 				// Se non metto nulla nel do-while, il Waiter non terminerà mai
 				// Mi basta mettere una stampa (o semplicemente usare `volatile`) e si risolve il problema
 				// Perchè questo? -> Java Memory Model
+			} while(!doneA);
+
+			System.out.println("Waiter A terminated");
+		}
+	}
+	
+	private class NotifierB extends Thread {
+		@Override
+		public void run() {
+			System.out.println("Notifier B started");
+			
+			try {
+				
+				Thread.sleep(5000);
+				
+				synchronized (Example01.this) {
+					// Prendo l'oggetto che esisteva quando sono stati creati NotifierB e WaiterB
+					doneB = true;
+				}
+					
+				System.out.println("Notified B");
+			} catch (InterruptedException e) {
+				System.out.println("Interrupted");
+				return;
+			}
+
+			System.out.println("Notifier B terminated");
+		}
+	}
+	
+	private class WaiterB extends Thread {
+		@Override
+		public void run() {
+			System.out.println("Waiter B started");
+		
+			boolean done;
+			
+			do {
+				synchronized (Example01.this) {
+					done = doneB;
+					// Viene copiato perche' e' un dato primitivo
+				}
 			} while(!done);
 
-			System.out.println("Waiter terminated");
+			System.out.println("Waiter B terminated");
+		}
+	}
+	
+	private class NotifierC extends Thread {
+		@Override
+		public void run() {
+			System.out.println("Notifier C started");
+			
+			try {
+				
+				Thread.sleep(5000);
+				
+				synchronized (Example01.this) {
+					doneC = true;
+					Example01.this.notifyAll();
+					// Se inverto queste due istruzioni nella sezione critica, non succede nulla
+					// perche' dentro la sezione critica ci sta un solo thread alla volta.
+					// Viceversa, se non siamo nella sezione critica, puo' provocare numerosi problemi.
+				}
+					
+				System.out.println("Notified C");
+			} catch (InterruptedException e) {
+				System.out.println("Interrupted C");
+				return;
+			}
+
+			System.out.println("Notifier C terminated");
+		}
+	}
+	
+	private class WaiterC extends Thread {
+		@Override
+		public void run() {
+			System.out.println("Waiter C started");
+
+			try {
+				Thread.sleep(8000);
+				
+				synchronized (Example01.this) {
+					// Se non metto questo controllo si perde il notifier (se tengo `Thread.sleep(8000);`
+					if(!doneC)
+						Example01.this.wait();
+				}
+			} catch (Exception e) {
+				System.out.println("Interrupted C");
+				return;
+			}
+
+			System.out.println("Waiter C terminated");
 		}
 	}
 }

2c97ee1671acdc71d4e888490870eefc5e680902
0xMere <merenda.s.mattia@gmail.com>
2023-11-11 09:41:06 +0100

    @merendamattia: aggiunti commenti codice

--- java/@merendamattia/Esempio_04/src/it/unipr/informatica/examples/Example04.java
+++ java/@merendamattia/Esempio_04/src/it/unipr/informatica/examples/Example04.java
@@ -1,23 +1,24 @@
 package it.unipr.informatica.examples;
 
 import java.util.function.UnaryOperator;
-
 import it.unipr.informatica.concurrent.atomic.AtomicReference;
 
 public class Example04 {
 	private void go() {
+		// Gestisce l'oggetto in mutua esclusione
 		AtomicReference<Integer> counter = new AtomicReference<>(1);
 
 		int i = counter.get();
 
-		Incrementer incrementer = new Incrementer();
+		Incrementer incrementer = new Incrementer(); // Creo un nuovo operatore unario
 
 		while (i <= 10) {
-			System.out.println("Using inner class " + i);
+			System.out.println("Using inner class: " + i);
 
 			i = counter.updateAndGet(incrementer);
 		}
 
+		// Classe interna anonima
 		UnaryOperator<Integer> operator = new UnaryOperator<Integer>() {
 			@Override
 			public Integer apply(Integer value) {
@@ -26,13 +27,13 @@ public class Example04 {
 		};
 
 		while (i <= 20) {
-			System.out.println("Using anonymous inner class " + i);
+			System.out.println("Using anonymous inner class: " + i);
 
 			i = counter.updateAndGet(operator);
 		}
 
 		while (i <= 30) {
-			System.out.println("Using lambda function " + i);
+			System.out.println("Using lambda function: " + i);
 
 			i = counter.updateAndGet((x) -> {
 				return x + 1;
@@ -40,13 +41,13 @@ public class Example04 {
 		}
 
 		while (i <= 40) {
-			System.out.println("Using lambda expression " + i);
+			System.out.println("Using lambda expression: " + i);
 
 			i = counter.updateAndGet((x) -> x + 1);
 		}
 
 		while (i <= 50) {
-			System.out.println("Using method reference " + i);
+			System.out.println("Using method reference: " + i);
 
 			i = counter.updateAndGet(this::increment);
 		}
@@ -60,6 +61,7 @@ public class Example04 {
 		new Example04().go();
 	}
 
+	// Classe interna
 	private static class Incrementer implements UnaryOperator<Integer> {
 		@Override
 		public Integer apply(Integer value) {

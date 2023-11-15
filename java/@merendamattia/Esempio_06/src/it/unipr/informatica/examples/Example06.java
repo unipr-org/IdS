a45c9c450f74be2802bdede36150ed3c81e6174b
0xMere <merenda.s.mattia@gmail.com>
2023-11-15 10:22:53 +0100

    @merendamattia: sistemato esempio 6

--- java/@merendamattia/Esempio_06/src/it/unipr/informatica/examples/Example06.java
+++ java/@merendamattia/Esempio_06/src/it/unipr/informatica/examples/Example06.java
@@ -1,24 +1,44 @@
 package it.unipr.informatica.examples;
 
-public class Example07 {
-	private void process(ResourceContent content) {
-		System.out.println("Downloaded " + content.getData().length + " bytes from " + content.getURL());
+import it.unipr.informatica.concurrent.ExecutionException;
+import it.unipr.informatica.concurrent.Future;
+
+public class Example06 {
+	private void process(Future<ResourceContent> future) {
+		try {
+			ResourceContent content = future.get();
+
+			System.out.println("Downloaded " + content.getData().length + " bytes from " + content.getURL());
+		} catch (ExecutionException executionException) {
+			System.err.println("Cannot download with error: " + executionException.getMessage());
+		} catch (InterruptedException interruptedException) {
+			System.err.println("Interrupted");
+
+			System.exit(-1);
+		}
 	}
 
 	private void go() {
 		DownloadManager downloadManager = new DownloadManager(4);
 
-		downloadManager.download("https://www.google.it", this::process);
-		downloadManager.download("https://www.youtube.it", this::process);
-		downloadManager.download("https://www.amazon.it", this::process);
-		downloadManager.download("https://www.missingwebsite.com", this::process);
-		downloadManager.download("https://www.ebay.it", this::process);
-		downloadManager.download("https://www.unipr.it", this::process);
+		Future<ResourceContent> future1 = downloadManager.download("https://www.google.it");
+		Future<ResourceContent> future2 = downloadManager.download("https://www.youtube.it");
+		Future<ResourceContent> future3 = downloadManager.download("https://www.amazon.it");
+		Future<ResourceContent> future4 = downloadManager.download("https://www.missingwebsite.com");
+		Future<ResourceContent> future5 = downloadManager.download("https://www.ebay.it");
+		Future<ResourceContent> future6 = downloadManager.download("https://www.unipr.it");
+
+		process(future1);
+		process(future2);
+		process(future3);
+		process(future4);
+		process(future5);
+		process(future6);
 
 		downloadManager.shutdown();
 	}
 
 	public static void main(String[] args) {
-		new Example07().go();
+		new Example06().go();
 	}
 }

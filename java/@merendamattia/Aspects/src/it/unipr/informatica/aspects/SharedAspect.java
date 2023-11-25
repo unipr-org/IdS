3fb0eca6a3a6a137182f96e35292aebcba635520
0xMere <merenda.s.mattia@gmail.com>
2023-11-25 10:36:46 +0100

    @merendamattia: aggiunta commenti per una comprensione migliore del codice

--- java/@merendamattia/Aspects/src/it/unipr/informatica/aspects/SharedAspect.java
+++ java/@merendamattia/Aspects/src/it/unipr/informatica/aspects/SharedAspect.java
@@ -10,48 +10,75 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+// Classe principale che rappresenta l'aspetto condiviso
 public class SharedAspect {
-	public static <T> T attach(T target) {
-		if (target == null)
-			throw new IllegalArgumentException("target == null");
 
-		Class<?> targetClass = target.getClass();
+    // Metodo per attaccare un oggetto a un aspetto condiviso
+    public static <T> T attach(T target) {
+    	
+        // Verifica se l'oggetto da attaccare non è nullo
+        if (target == null)
+            throw new IllegalArgumentException("target == null");
 
-		Class<?>[] targetInterfaces = targetClass.getInterfaces();
+        // Ottiene la classe dell'oggetto target
+        Class<?> targetClass = target.getClass();
 
-		Object proxy = Proxy.newProxyInstance(targetClass.getClassLoader(), targetInterfaces,
-				new InnerInvocationHandler(target));
+        // Ottiene le interfacce implementate dall'oggetto target
+        Class<?>[] targetInterfaces = targetClass.getInterfaces();
 
-		@SuppressWarnings("unchecked")
-		T result = (T) proxy;
+        // Crea un proxy dinamico che implementa le interfacce dell'oggetto target
+        Object proxy = Proxy.newProxyInstance(targetClass.getClassLoader(), targetInterfaces,
+                new InnerInvocationHandler(target));
 
-		return result;
-	}
+        // Esegue un cast dell'oggetto proxy al tipo desiderato
+        @SuppressWarnings("unchecked")
+        T result = (T) proxy;
 
-	private static class InnerInvocationHandler implements InvocationHandler {
-		private Object target;
+        // Restituisce l'oggetto proxy attaccato all'aspetto condiviso
+        return result;
+    }
 
-		private Object lock;
+    // Classe interna che funge da gestore delle chiamate ai metodi sull'oggetto proxy
+    private static class InnerInvocationHandler implements InvocationHandler {
+        
+    	// Oggetto target a cui le chiamate ai metodi saranno delegate
+        private Object target;
 
-		private InnerInvocationHandler(Object target) {
-			this.target = target;
+        // Oggetto di sincronizzazione per garantire l'accesso sincronizzato all'oggetto target
+        private Object lock;
 
-			this.lock = new Object();
-		}
+        // Costruttore che inizializza l'oggetto target e il lock di sincronizzazione
+        private InnerInvocationHandler(Object target) {
+            this.target = target;
 
-		@Override
-		public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
-			try {
-				synchronized (lock) {
-					Object result = method.invoke(target, arguments);
+            // Utilizza un oggetto separato come lock di sincronizzazione
+            // per evitare problemi legati a lock imprevisti
+            this.lock = new Object();
+        }
 
-					return result;
-				}
-			} catch (InvocationTargetException exception) {
-				throw exception.getCause();
-			} catch (Throwable throwable) {
-				throw throwable;
-			}
-		}
-	}
+        // Metodo chiamato quando viene invocato un metodo sull'oggetto proxy
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+            try {
+                
+            	// Utilizza il lock per garantire l'accesso sincronizzato all'oggetto target
+                synchronized (lock) {
+                
+                	// Invoca il metodo sul target originale e ottiene il risultato
+                    Object result = method.invoke(target, arguments);
+
+                    // Restituisce il risultato ottenuto
+                    return result;
+                }
+            } catch (InvocationTargetException exception) {
+                
+            	// Gestisce eventuali eccezioni lanciate all'interno del metodo invocato
+                throw exception.getCause();
+            } catch (Throwable throwable) {
+                
+            	// Gestisce altre eccezioni
+                throw throwable;
+            }
+        }
+    }
 }

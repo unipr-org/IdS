90ff1166b943c56cd926d4e53125812e8bfe386e
0xMere <merenda.s.mattia@gmail.com>
2023-12-13 12:20:08 +0100

    @merendamattia: aggiunto PersistentAspect

---
+++ java/@merendamattia/Ripasso/src/it/unipr/informatica/aspects/PersistentAspectTest.java
@@ -0,0 +1,46 @@
+package it.unipr.informatica.aspects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersistentAspectTest {
+	private void printAndAddIntegers(List<Integer> list) {
+		printIntegers(list);
+		
+		for(int i = 0; i < 3; ++i) 
+			list.add((int) (Math.random() * 100));
+	}
+	
+	private void printIntegers(List <Integer> list) {
+		if (!list.isEmpty()) {
+			System.out.println("Current integers:");
+		
+			for(Integer interi : list)
+				System.out.println(interi);
+		}
+	}
+	
+	private void go() {
+		try {
+			PersistentHandler<ArrayList<Integer>> integerHandler = PersistentAspect.attach("src/it/unipr/informatica/aspects/backup/Integers.dat", new ArrayList<Integer>());
+			
+			List<Integer> interi = integerHandler.get();
+		
+			printAndAddIntegers(interi);
+			
+			integerHandler.commit();
+			
+			System.out.println("integers saved");
+			
+			printIntegers(interi);
+			
+			
+		} catch(Throwable throwable) {
+			throwable.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new PersistentAspectTest().go();
+	}
+}

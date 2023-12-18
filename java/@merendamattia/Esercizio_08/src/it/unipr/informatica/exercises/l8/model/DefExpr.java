fead6daf8b87220c25a0107b968f8353e690bfd1
0xMere <merenda.s.mattia@gmail.com>
2023-12-18 10:05:54 +0100

    @merendamattia: esercizio 8 visto a lezione (da terminare)

--- java/@merendamattia/Esercizio_08/src/it/unipr/informatica/exercises/l8/model/DefExpr.java
+++ java/@merendamattia/Esercizio_08/src/it/unipr/informatica/exercises/l8/model/DefExpr.java
@@ -3,5 +3,4 @@ package it.unipr.informatica.exercises.l8.model;
 public interface DefExpr extends GlobalExpr {
 	public Id getName();
 	public Expr getValue();
-
 }
--- java/@merendamattia/Esercizio_08/src/it/unipr/informatica/exercises/l8/model/SimpleL8Factory.java
+++ java/@merendamattia/Esercizio_08/src/it/unipr/informatica/exercises/l8/model/SimpleL8Factory.java
@@ -23,32 +23,27 @@ public class SimpleL8Factory implements L8Factory {
 	
 	@Override
 	public DefExpr newDefExpr(Id name, Expr value) {
-		// TODO Auto-generated method stub
-		return null;
+		return new InnerDefExpr(name, value);
 	}
 
 	@Override
 	public CondExpr newCondExpr(Expr condition, Expr then, Expr otherwise) {
-		// TODO Auto-generated method stub
-		return null;
+		return new InnerCondExpr(condition, then, otherwise);
 	}
 
 	@Override
 	public EvalExpr newEvalExpr(Expr functor, Expr argument) {
-		// TODO Auto-generated method stub
-		return null;
+		return new InnerEvalExpr(functor, argument);
 	}
 
 	@Override
 	public AddExpr newAddExpr(Expr left, Expr right) {
-		// TODO Auto-generated method stub
-		return null;
+		return new InnerAddExpr(left, right);
 	}
 
 	@Override
 	public SubtractExpr newSubtractExpr(Expr left, Expr right) {
-		// TODO Auto-generated method stub
-		return null;
+		return new InnerSubtractExpr(left, right);
 	}
 
 	@Override
@@ -72,15 +67,90 @@ public class SimpleL8Factory implements L8Factory {
 	}
 	
 	// ------------------------------------------ Inner Classes
-	private class InnerMultiplyExpr implements MultiplyExpr {
+	private class InnerDefExpr implements DefExpr {
+		private Id name;
+		private Expr value;
+		
+		public InnerDefExpr(Id name, Expr value) {
+			if(name == null || value == null)
+				throw new IllegalArgumentException("name == null || value == null");
+			
+			this.name = name;
+			this.value = value;
+		}
+
+		@Override
+		public Id getName() { return name; }
+
+		@Override
+		public Expr getValue() { return value; }
+		
+		@Override
+		public void accept(Visitor visitor) {
+			// TODO Auto-generated method stub	
+		}
+	} // ! InnerDefExpr
+	
+	private class InnerCondExpr implements CondExpr {
+		private Expr condition;
+		private Expr then;
+		private Expr otherwise;
+
+		public InnerCondExpr(Expr condition, Expr then, Expr otherwise) {
+			if(condition == null || then == null || otherwise == null)
+				throw new IllegalArgumentException("condition == null || then == null || otherwise == null");
+			
+			this.condition = condition;
+			this.then = then;
+			this.otherwise = otherwise;
+		}
+		
+		@Override
+		public Expr getCondition() { return condition; }
+
+		@Override
+		public Expr getThen() { return then; }
+
+		@Override
+		public Expr getElse() { return otherwise; }
+		
+		@Override
+		public void accept(Visitor visitor) {
+			// TODO Auto-generated method stub
+		}
+	} // ! InnerCondExpr
+	
+	private class InnerEvalExpr implements EvalExpr {
+		private Expr functor;
+		private Expr argument;
+
+		public InnerEvalExpr(Expr functor, Expr argument) {
+			if(functor == null || argument == null)
+				throw new IllegalArgumentException("functor == null || argument == null");
+			
+			this.functor = functor;
+			this.argument = argument;
+		}
+
+		@Override
+		public Expr getFunctor() { return functor; }
+
+		@Override
+		public Expr getArgument() { return argument; }
+		
+		@Override
+		public void accept(Visitor visitor) {
+			// TODO Auto-generated method stub
+		}
+	} // ! InnerEvalExpr
+	
+	private class InnerAddExpr implements AddExpr {
 		private Expr left;
 		private Expr right;
 		
-		public InnerMultiplyExpr(Expr left, Expr right) {
-			if(left == null)
-				throw new NullPointerException("left == null");
-			if(right == null)
-				throw new NullPointerException("right == null");
+		public InnerAddExpr(Expr left, Expr right) {
+			if(left == null || right == null)
+				throw new IllegalArgumentException("left == null || right == null");
 			
 			this.left = left;
 			this.right = right;
@@ -97,7 +167,56 @@ public class SimpleL8Factory implements L8Factory {
 			// TODO Auto-generated method stub
 			
 		}
+	} // ! InnerAddExpr
+	
+	private class InnerSubtractExpr implements SubtractExpr {
+		private Expr left;
+		private Expr right;
 		
+		public InnerSubtractExpr(Expr left, Expr right) {
+			if(left == null || right == null)
+				throw new IllegalArgumentException("left == null || right == null");
+			
+			this.left = left;
+			this.right = right;
+		}
+		
+		@Override
+		public Expr getLeft() { return left; }
+
+		@Override
+		public Expr getRight() { return right; }
+
+		@Override
+		public void accept(Visitor visitor) {
+			// TODO Auto-generated method stub
+			
+		}
+	} // ! InnerSubtractExpr
+	
+	private class InnerMultiplyExpr implements MultiplyExpr {
+		private Expr left;
+		private Expr right;
+		
+		public InnerMultiplyExpr(Expr left, Expr right) {
+			if(left == null || right == null)
+				throw new IllegalArgumentException("left == null || right == null");
+			
+			this.left = left;
+			this.right = right;
+		}
+		
+		@Override
+		public Expr getLeft() { return left; }
+
+		@Override
+		public Expr getRight() { return right; }
+
+		@Override
+		public void accept(Visitor visitor) {
+			// TODO Auto-generated method stub
+			
+		}
 	} // ! InnerMultiplyExpr
 	
 	private class InnerFunc implements Func {
@@ -105,10 +224,8 @@ public class SimpleL8Factory implements L8Factory {
 		private Expr body;
 		
 		public InnerFunc(Id argument, Expr body) {
-			if(argument == null)
-				throw new NullPointerException("argument == null");
-			if(body == null)
-				throw new NullPointerException("body == null");
+			if(body == null || argument == null)
+				throw new IllegalArgumentException("body == null || argument == null");
 			
 			this.argument = argument;
 			this.body = body;
@@ -147,7 +264,7 @@ public class SimpleL8Factory implements L8Factory {
 		
 		public InnerId(String value) {
 			if(value == null)
-				throw new NullPointerException("value == null");
+				throw new IllegalArgumentException("value == null");
 			this.value = value;
 		}
 		

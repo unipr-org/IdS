352d06b0a910885226242c90ca74f385814c54a8
manueldiagostino <diagostinomanuel@gmail.com>
2023-12-22 18:03:48 +0100

    @manueldiagostino: aggiunta cartella esercizi in LaTeX e alcune implementazioni

---
+++ java/@manueldiagostino/Esercizi/src/it/unipr/informatica/esercizi/esercizio_111/District.java
@@ -0,0 +1,60 @@
+package it.unipr.informatica.esercizi.esercizio_111;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import it.unipr.informatica.esercizi.esercizio_111.Abstractions.Node;
+import it.unipr.informatica.esercizi.esercizio_111.Abstractions.NodeComposite;
+
+/**
+ * @author Di Agostino Manuel
+ * https://github.com/manueldiagostino
+ */
+public class District extends NodeComposite {
+	protected Collection<Node> _children;
+	
+	public District(String name) {
+		this._name = name;
+		this._type = NodeType.DISTRICT;
+		this._children = new LinkedList<Node>();
+	}
+	
+	@Override
+	public void add(Node node) {
+		this._children.add(node);
+	}
+
+	@Override
+	public Collection<Node> remove(String name) {
+		LinkedList<Node> res = new LinkedList<Node>();
+		
+		for (Node n : _children)
+			if (n.getName().equals(name)) {
+				res.add(n);
+				_children.remove(n);
+			}
+		
+		return res;
+	}
+
+	@Override
+	public Collection<Node> getChildren() {
+		return this._children;
+	}
+	
+	@Override
+	public NodeType getType() {
+		return this._type;
+	}
+
+	@Override
+	public String getName() {
+		return this._name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this._name = name;
+	}
+
+}
---
+++ java/@manueldiagostino/Esercizi/src/it/unipr/informatica/esercizi/esercizio_111/Main.java
@@ -0,0 +1,169 @@
+package it.unipr.informatica.esercizi.esercizio_111;
+import java.util.Collection;
+
+import it.unipr.informatica.esercizi.esercizio_111.Abstractions.*;
+import it.unipr.informatica.esercizi.esercizio_111.Iterators.Iterator;
+import it.unipr.informatica.esercizi.esercizio_111.Iterators.NodeBreadthFirstIterator;
+import it.unipr.informatica.esercizi.esercizio_111.Iterators.NodeDeepFirstIterator;
+import it.unipr.informatica.esercizi.esercizio_111.Iterators.NodeSpecificTypeIterator;
+
+/**
+ * @author Di Agostino Manuel
+ * https://github.com/manueldiagostino
+ */
+public class Main {
+	public void test01() {
+Node root = new Country("Country 1");
+		
+		for (int i=0; i<10; ++i) {
+			if (root.getComposite() != null) {
+				NodeComposite nodeC = (NodeComposite)(root);
+				if (i!=5)
+					nodeC.add(new Leaf("Leaf " + i));
+				else
+					nodeC.add(new District("District " + i));
+			}
+		}
+
+		System.out.println(root);
+		NodeComposite nodeC;
+		
+		if (root.getComposite() != null)
+			nodeC = (NodeComposite)(root);
+		else return;
+		
+		Collection<Node> children = nodeC.getChildren();
+		System.out.println("\nRoot's children:");
+		for (Node n : children)
+			System.out.println(n);
+	}
+	
+	public void testDeepVisit() {
+		NodeComposite root = new Country("Italia");
+		NodeComposite abruzzo = new Region("Abruzzo");
+		NodeComposite calabria = new Region("Calabria");
+		NodeComposite emilia = new Region("Emilia Romagna");
+		root.add(abruzzo);
+		root.add(calabria);
+		root.add(emilia);
+		
+		abruzzo.add(new Leaf("Teramo"));
+		abruzzo.add(new Leaf("Chieti"));
+		abruzzo.add(new Leaf("Pescara"));
+		abruzzo.add(new Leaf("L'Aquila"));
+		
+		emilia.add(new Leaf("Parma"));
+		
+		calabria.add(new Leaf("Catanzaro"));
+		calabria.add(new Leaf("Catanzaro Lido"));
+	
+		System.out.println("Albero:");
+		Iterator it = new NodeDeepFirstIterator(root);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		calabria.remove("Catanzaro");
+		System.out.println("\nAlbero post distruzione di Catanzaro:");
+		it = new NodeDeepFirstIterator(root);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("\nAlbero parziale (Abruzzo):");
+		it = new NodeDeepFirstIterator(abruzzo);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("\nAlbero con solo una foglia (Bologna):");
+		Node bologna = new Leaf("Bologna");
+		it = new NodeDeepFirstIterator(bologna);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+	}
+	
+	public void testBreadthVisit() {
+		NodeComposite root = new Country("Italia");
+		NodeComposite abruzzo = new Region("Abruzzo");
+		NodeComposite calabria = new Region("Calabria");
+		NodeComposite emilia = new Region("Emilia Romagna");
+		root.add(abruzzo);
+		root.add(calabria);
+		root.add(emilia);
+		
+		abruzzo.add(new Leaf("Teramo"));
+		abruzzo.add(new Leaf("Chieti"));
+		abruzzo.add(new Leaf("Pescara"));
+		abruzzo.add(new Leaf("L'Aquila"));
+		
+		emilia.add(new Leaf("Parma"));
+		
+		calabria.add(new Leaf("Catanzaro"));
+		calabria.add(new Leaf("Catanzaro Lido"));
+	
+		System.out.println("Albero:");
+		Iterator it = new NodeBreadthFirstIterator(root);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		calabria.remove("Catanzaro");
+		System.out.println("\nAlbero post distruzione di Catanzaro:");
+		it = new NodeBreadthFirstIterator(root);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("\nAlbero parziale (Abruzzo):");
+		it = new NodeBreadthFirstIterator(abruzzo);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("\nAlbero con solo una foglia (Bologna):");
+		Node bologna = new Leaf("Bologna");
+		it = new NodeBreadthFirstIterator(bologna);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}	
+	}
+	
+	public void testSpecificNodeType() {
+		NodeComposite root = new Country("Italia");
+		NodeComposite abruzzo = new Region("Abruzzo");
+		NodeComposite calabria = new Region("Calabria");
+		NodeComposite emilia = new Region("Emilia Romagna");
+		root.add(abruzzo);
+		root.add(calabria);
+		root.add(emilia);
+		
+		abruzzo.add(new Leaf("Teramo"));
+		abruzzo.add(new Leaf("Chieti"));
+		abruzzo.add(new Leaf("Pescara"));
+		abruzzo.add(new Leaf("L'Aquila"));
+		
+		emilia.add(new Leaf("Parma"));
+		
+		calabria.add(new Leaf("Catanzaro"));
+		calabria.add(new Leaf("Catanzaro Lido"));
+	
+		System.out.println("Elementi di tipo LEAF:");
+		Iterator it = new NodeSpecificTypeIterator(root, NodeType.LEAF);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("Elementi di tipo REGION:");
+		it = new NodeSpecificTypeIterator(root, NodeType.REGION);
+		while (!it.isDone()) {
+			System.out.println(it.next());
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Main().testSpecificNodeType();
+	}
+}

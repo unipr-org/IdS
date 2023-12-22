219fcd4ffbe3e144e342be5233e6680c1cd9f5e4
0xMere <merenda.s.mattia@gmail.com>
2023-12-22 13:03:47 +0100

    @merendamattia: esercitazione Composite

---
+++ java/@merendamattia/Ripasso/src/it/unipr/informatica/exercises/esercizio1_1_1/Composite.java
@@ -0,0 +1,9 @@
+package it.unipr.informatica.exercises.esercizio1_1_1;
+
+import java.util.Collection;
+
+public abstract class Composite extends Component {
+	protected Collection<Component> children;
+	
+	public abstract Collection<Component> getChildren();
+}
---
+++ java/@merendamattia/Ripasso/src/it/unipr/informatica/exercises/esercizio1_1_1/Main.java
@@ -0,0 +1,44 @@
+package it.unipr.informatica.exercises.esercizio1_1_1;
+
+public class Main {
+	private void go() {
+		Component l1 = new Leaf("Soverato");
+		Component l2 = new Leaf("Sellia Marina");
+		Component l3 = new Leaf("Montepaone");
+		Component l4 = new Leaf("Albinea");
+		Component l5 = new Leaf("Rivalta");
+		Component l6 = new Leaf("Reggiolo");
+		
+		Component d1 = new District("Catanzaro");
+		Component d2 = new District("Reggio Emilia");
+		
+		Component r1 = new Region("Calabria");
+		Component r2 = new Region("Emilia Romagna");
+		
+		Component c1 = new Country("Italia");
+		
+		d1.add(l1);
+		d1.add(l2);
+		d1.add(l3);
+		r1.add(d1);
+		c1.add(r1);
+		
+		d2.add(l4);
+		d2.add(l5);
+		d2.add(l6);
+		r2.add(d2);
+		c1.add(r2);
+		
+		System.out.println(c1.print());
+		
+		r2.remove(d2);
+		
+		System.out.println("After remove...");
+		System.out.println(c1.print());
+		
+	}
+	
+	public static void main(String[] args) {
+		new Main().go();
+	}
+}

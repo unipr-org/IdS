8da44655cd394b28595784d4f8881c08d401a264
manueldiagostino <diagostinomanuel@gmail.com>
2023-12-29 17:01:08 +0100

    @manueldiagostino: svolgimento esame 20220914

---
+++ java/@manueldiagostino/Ripasso/src/it/unipr/informatica/exams/exam_20220914/Worker.java
@@ -0,0 +1,10 @@
+package it.unipr.informatica.exams.exam_20220914;
+
+/**
+ * @author Di Agostino Manuel
+ * https://github.com/manueldiagostino
+ */
+public interface Worker {
+	public void run();
+	public void stop();
+}

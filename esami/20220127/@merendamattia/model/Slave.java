6e81227759646efb956915e26b64266a72299a1e
0xMere <merenda.s.mattia@gmail.com>
2024-01-05 11:37:23 +0100

    @merendamattia: esame completo

---
+++ esami/20220127/@merendamattia/model/Slave.java
@@ -0,0 +1,8 @@
+package it.unipr.informatica.exams.exam_20220127.model;
+
+public interface Slave {
+	public void executePartA();
+	public void executePartB();
+	public void executePartC();
+	public int finish();
+}
